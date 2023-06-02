2aa41e1688da0224e03b969441858813e54dbcde
pablomahumane7 <117922241+pablomahumane7@users.noreply.github.com>
2023-06-02 12:55:34 +0200

    inicial Commit

--- app/src/main/java/com/example/gerenciamento_de_transito/MainActivity.java
+++ app/src/main/java/com/example/gerenciamento_de_transito/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.gerenciamento_de_transito;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.gerenciamento_de_transito.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +24,64 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,binding.drawerLayout,binding.appBarMain.toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.containers,new HomeFragment());
+        transaction.commit();
+
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                int id = item.getItemId();
+
+                if (id==R.id.home) {
+
+                    transaction.replace(R.id.containers,new HomeFragment());
+                    transaction.commit();
+                } else if (id==R.id.fav) {
+
+                    transaction.replace(R.id.containers,new FavoritosFragment());
+                    transaction.commit();
+
+                } else if (id==R.id.share) {
+
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_SEND);
+                    intent.putExtra(Intent.EXTRA_TEXT, "https://www.youtube.com/watch?v=N9O2RabbAqE");
+                    intent.setType("text/plain");
+                    startActivity(intent);
+
+                }
+
+                binding.drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
     }
 }
